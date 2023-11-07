08f074935505d2ac99a2c2b526df4dc8010751cc
Bashorun Olajide John <bashorunolajide@gmail.com>
2023-11-07 23:10:14 +0100

    Add google client id for signing in with OAuth Client ID

--- app/src/main/java/com/project/wediary/util/Constants.kt
+++ app/src/main/java/com/project/wediary/util/Constants.kt
@@ -3,4 +3,5 @@ package com.project.wediary.util
 object Constants {
 
     const val APP_ID = "mydiaryapp-kebfi"
+    const val CLIENT_ID = "792448984430-vspfbg9u1h6sjc87gfbuhm02ati5fhs5.apps.googleusercontent.com"
 }
